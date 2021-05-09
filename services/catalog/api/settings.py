c4cae1178954aba952f30929a39a27378b20c30d
Alex Drobinin <31101295+drobiAlex@users.noreply.github.com>
2021-05-09 22:34:55 +0300

    Docker services config

--- services/catalog/api/settings.py
+++ services/catalog/api/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'e$iy^w5v@gsnbw2olcz7mus1b-n)2aj5(som)i@qhh)on90&po'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- services/user/api/settings.py
+++ services/user/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')ym5l%ey*pi(6uzvqq%w_q4yas1)cloq8@184e*sn-inimbtm^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
