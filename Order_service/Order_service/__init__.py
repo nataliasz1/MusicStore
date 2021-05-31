ad515eef72c7b6092e4693290b419cde6c6e0fbe
kubix97 <51679705+kubix97@users.noreply.github.com>
2021-06-01 00:39:35 +0200

    Order service added

---
+++ Order_service/Order_service/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for Order_service.
+"""
---
+++ Order_service/Order_service/urls.py
@@ -0,0 +1,20 @@
+"""
+Order_service URL Configuration
+
+"""
+
+# Uncomment next two lines to enable admin:
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'orders', views.OrderViewSet)
+router.register(r'orderitems', views.OrderItemViewSet)
+router.register(r'payments', views.PaymentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls)
+]
