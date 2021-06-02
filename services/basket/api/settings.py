ed86e1178bca517a0c8968cf83ab8ca3de9bbee6
MJonca <mjonca@mion.elka.pw.edu.pl>
2021-06-02 20:21:36 +0200

    Added basket to docker-compose

--- services/basket/api/settings.py
+++ services/basket/api/settings.py
@@ -96,25 +96,25 @@ print(POSTGRES_PASSWORD)
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': POSTGRES_DB,
-        'USER': POSTGRES_USER,
-        'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': POSTGRES_HOST,
-        'PORT': 5432, 
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': POSTGRES_DB,
+#         'USER': POSTGRES_USER,
+#         'PASSWORD': POSTGRES_PASSWORD,
+#         'HOST': POSTGRES_HOST,
+#         'PORT': 5432,
+#     }
+# }
 
 
-""" 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+}
 
 
 # Password validation
